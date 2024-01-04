537f8a2cb482d793b9f0114849300443371f92bf
mrattcs <j.t.k.hashemi@gmail.com>
2024-01-04 19:48:29 +0100

    added messageresponse

---
+++ src/main/java/com/mrattcs/instagramapi/response/MessageResponse.java
@@ -0,0 +1,14 @@
+package com.mrattcs.instagramapi.response;
+
+public class MessageResponse {
+
+    private String message;
+
+    public MessageResponse() {
+    }
+
+    public MessageResponse(String message) {
+        super();
+        this.message = message;
+    }
+}
